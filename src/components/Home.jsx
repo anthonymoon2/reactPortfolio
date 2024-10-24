6b9023b22b305e47437217cad63b69f05274a4ee
Anthony Moon <amoon@chapman.edu>
2024-10-24 00:57:31 -0700

    fixed route on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
@@ -15,12 +16,12 @@ function Home() {
         </p>
 
         {/* Buttons for projects and contact */}
-        <a href="/projects">
+        <Link to="/projects">
           <button className="dark-button">Projects</button>
-        </a>
-        <a href="/contact">
+        </Link>
+        <Link to="/contact">
           <button className="light-button">Contact</button>
-        </a>
+        </Link>
       </section>
     </main>
   );
